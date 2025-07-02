9cd09ea8ff9c09590c372edc7bf60cc91b997da7
Giga Mania <hil.giga@gmail.com>
2025-07-02 23:04:01 +0100

    feat(user/auth): add request input validations to user auth routes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { AuthController } from "../controllers/AuthController";
 import { AuthServiceImpl } from "../services/AuthServiceImpl";
 import { PrismaUserRepository } from "../infrastructure/repositories/PrismaUserRepository";
+import { validateRequestInput } from "../middleware/validateRequestInput.middleware";
+import { createUserSchema, loginUserSchema } from "../validators/user.validator";
 
 
 const router = Router();
@@ -10,7 +12,7 @@ const repository = new PrismaUserRepository();
 const service = new AuthServiceImpl(repository);
 const controller = new AuthController(service);
 
-router.post("/register", controller.register.bind(controller));
-router.post("/login", controller.login.bind(controller));
+router.post("/register", validateRequestInput(createUserSchema), controller.register.bind(controller));
+router.post("/login", validateRequestInput(loginUserSchema), controller.login.bind(controller));
 
 export default router;
---
+++ src/validators/user.validator.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+
+export const createUserSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+  name: z.string().min(4)
+}).strict();
+
+export const loginUserSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6)
+}).strict();
