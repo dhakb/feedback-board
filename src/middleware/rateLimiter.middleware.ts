212e3e361986c59fc551e29826515fcac3608a57
Giga Mania <hil.giga@gmail.com>
2025-08-03 09:24:53 +0100

    feat: increase enpoints request limit per ms windows

--- src/middleware/rateLimiter.middleware.ts
+++ src/middleware/rateLimiter.middleware.ts
@@ -3,7 +3,7 @@ import rateLimit from "express-rate-limit";
 
 export const strictLimiter = rateLimit({
   windowMs: 60 * 1000,   // 1 minutes
-  limit: 20,             // Limit each IP to X requests per `windowMs`
+  limit: 30,             // Limit each IP to X requests per `windowMs`
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false,  // Disable the `X-RateLimit-*` headers
   message: {message: "Too many requests, please try again later."}
